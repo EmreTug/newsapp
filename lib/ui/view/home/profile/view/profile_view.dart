8e38dc8fd2c785e79d9e22061b20f3b2fe7c6ceb
Emre Tuğ <emree68.12@gmail.com>
2023-01-26 17:18:32 +0300

    profile view update

--- lib/ui/view/home/profile/view/profile_view.dart
+++ lib/ui/view/home/profile/view/profile_view.dart
@@ -48,161 +48,180 @@ class ProfileView extends StatelessWidget {
           padding: const EdgeInsets.symmetric(
             horizontal: UIHelper.HorizontalSpaceMedium,
           ),
-          child: FutureBuilder(
-            future: model.getUserProfile(),
-            builder: (context, snapshot) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      CircleAvatar(
-                        backgroundImage: AssetImage("profile".getimage()),
-                        radius: 50,
-                      ),
-                      Column(
-                        children: const [
-                          CustomText(
-                              text: "2196", fontSize: 16, weight: FontWeight.w600),
-                          CustomText(
-                            text: "Followers",
-                            fontSize: 16,
-                            weight: FontWeight.w400,
-                            color: Color(0xff4E4B66),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: const [
-                          CustomText(
-                              text: "567", fontSize: 16, weight: FontWeight.w600),
-                          CustomText(
-                            text: "Following",
-                            fontSize: 16,
-                            weight: FontWeight.w400,
-                            color: Color(0xff4E4B66),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: const [
-                          CustomText(
-                              text: "21", fontSize: 16, weight: FontWeight.w600),
-                          CustomText(
-                            text: "News",
-                            fontSize: 16,
-                            weight: FontWeight.w400,
-                            color: Color(0xff4E4B66),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(top: 16),
-                    child: CustomText(
-                        text: "Wilson Franci",
-                        fontSize: 16,
-                        weight: FontWeight.w600),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(bottom: 16),
-                    child: CustomText(
-                      text:
-                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                      fontSize: 16,
-                      weight: FontWeight.w400,
-                      color: Color(0xff4E4B66),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      SizedBox(
-                          width: (MediaQuery.of(context).size.width / 2) - 40,
-                          child: CustomButtonFullWidth(
-                            text: "Edit Profile",
-                            click: () {
-                              Navigator.pushNamed(context, "editprofile");
-                            },
-                          )),
-                      SizedBox(
-                          width: (MediaQuery.of(context).size.width / 2) - 40,
-                          child: const CustomButtonFullWidth(
-                            text: "Website",
-                          )),
-                    ],
-                  ),
-                  FutureBuilder(
-                      future: model.getData(),
-                      builder: (context, snapshot) {
-                        return Flexible(
-                          child: DefaultTabController(
-                            length: 2,
-                            child: Scaffold(
-                              appBar: AppBar(
-                                toolbarHeight: 10,
-                                bottom: const TabBar(
-                                  labelColor: Colors.black,
-                                  isScrollable: true,
-                                  tabs: [
-                                    Tab(
-                                      text: "News",
-                                    ),
-                                    Tab(
-                                      text: "Recent",
-                                    ),
-                                  ],
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              FutureBuilder(
+                  future: model.getUserProfile(),
+                  builder: (context, snapshot) {
+                        if(snapshot.connectionState==ConnectionState.waiting){
+                          return const Center(child: CircularProgressIndicator(),);
+                        }
+                        else {
+                                return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            CircleAvatar(
+                              backgroundImage: NetworkImage(snapshot.data!["image"]),
+                              radius: 50,
+                            ),
+                            Column(
+                              children: const [
+                                CustomText(
+                                    text: "0",
+                                    fontSize: 16,
+                                    weight: FontWeight.w600),
+                                CustomText(
+                                  text: "Followers",
+                                  fontSize: 16,
+                                  weight: FontWeight.w400,
+                                  color: Color(0xff4E4B66),
                                 ),
-                              ),
-                              body: TabBarView(
-                                children: [
-                                  snapshot.data != null
-                                      ? ListView.builder(
-                                          itemCount: snapshot.data!.docs.length,
-                                          itemBuilder: (context, index) {
-                                            return NewsWidget(
-                                                id: snapshot.data!.docs[index].id,
-                                                imagePath: snapshot
-                                                    .data!.docs[index]["photoUrl"],
-                                                country: "Europe",
-                                                title: snapshot.data!.docs[index]
-                                                    ["title"],
-                                                authorlogoPath: "logo",
-                                                author: "BBC News");
-                                          },
-                                        )
-                                      : const Center(
-                                          child: CircularProgressIndicator(),
-                                        ),
-                             snapshot.data != null
-                                      ? ListView.builder(
-                                    itemCount: snapshot.data!.docs.length,
-                                    itemBuilder: (context, index) {
-                                      return  LatestCard(
-                                        id: snapshot.data!.docs[index].id,
+                              ],
+                            ),
+                            Column(
+                              children: const [
+                                CustomText(
+                                    text: "0",
+                                    fontSize: 16,
+                                    weight: FontWeight.w600),
+                                CustomText(
+                                  text: "Following",
+                                  fontSize: 16,
+                                  weight: FontWeight.w400,
+                                  color: Color(0xff4E4B66),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: const [
+                                CustomText(
+                                    text: "0",
+                                    fontSize: 16,
+                                    weight: FontWeight.w600),
+                                CustomText(
+                                  text: "News",
+                                  fontSize: 16,
+                                  weight: FontWeight.w400,
+                                  color: Color(0xff4E4B66),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                         Padding(
+                          padding: const EdgeInsets.only(top: 16),
+                          child: CustomText(
+                              text:snapshot.data!["FullName"],
+                              fontSize: 16,
+                              weight: FontWeight.w600),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(bottom: 16),
+                          child: CustomText(
+                            text:
+                                "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                            fontSize: 16,
+                            weight: FontWeight.w400,
+                            color: Color(0xff4E4B66),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SizedBox(
+                                width: (MediaQuery.of(context).size.width / 2) -
+                                    40,
+                                child: CustomButtonFullWidth(
+                                  text: "Edit Profile",
+                                  click: () {
+                                    Navigator.pushNamed(context, "editprofile");
+                                  },
+                                )),
+                            SizedBox(
+                                width: (MediaQuery.of(context).size.width / 2) -
+                                    40,
+                                child: const CustomButtonFullWidth(
+                                  text: "Website",
+                                )),
+                          ],
+                        ),
+                      ],
+                    );
+         
+                        }
+                       }),
+              FutureBuilder(
+                  future: model.getData(),
+                  builder: (context, snapshot) {
+                    return Flexible(
+                      child: DefaultTabController(
+                        length: 2,
+                        child: Scaffold(
+                          appBar: AppBar(
+                            toolbarHeight: 10,
+                            bottom: const TabBar(
+                              labelColor: Colors.black,
+                              isScrollable: true,
+                              tabs: [
+                                Tab(
+                                  text: "News",
+                                ),
+                                Tab(
+                                  text: "Recent",
+                                ),
+                              ],
+                            ),
+                          ),
+                          body: TabBarView(
+                            children: [
+                              snapshot.data != null
+                                  ? ListView.builder(
+                                      itemCount: snapshot.data!.docs.length,
+                                      itemBuilder: (context, index) {
+                                        return NewsWidget(
+                                            id: snapshot.data!.docs[index].id,
+                                            imagePath: snapshot
+                                                .data!.docs[index]["photoUrl"],
+                                            country: "Europe",
+                                            title: snapshot.data!.docs[index]
+                                                ["title"],
+                                            authorlogoPath: "logo",
+                                            author: "BBC News");
+                                      },
+                                    )
+                                  : const Center(
+                                      child: CircularProgressIndicator(),
+                                    ),
+                              snapshot.data != null
+                                  ? ListView.builder(
+                                      itemCount: snapshot.data!.docs.length,
+                                      itemBuilder: (context, index) {
+                                        return LatestCard(
+                                          id: snapshot.data!.docs[index].id,
                                           country: "country",
                                           authorLogo: "logo",
                                           title: snapshot.data!.docs[index]
-                                                    ["title"],
+                                              ["title"],
                                           authorName: "authorName",
-                                          imagePath: snapshot
-                                                    .data!.docs[index]["photoUrl"],);
-                                    },
-                                  )
-                                      : const Center(
-                                          child: CircularProgressIndicator(),
-                                        ),
-                                ],
-                              ),
-                            ),
+                                          imagePath: snapshot.data!.docs[index]
+                                              ["photoUrl"],
+                                        );
+                                      },
+                                    )
+                                  : const Center(
+                                      child: CircularProgressIndicator(),
+                                    ),
+                            ],
                           ),
-                        );
-                      }),
-                ],
-              );
-            }
+                        ),
+                      ),
+                    );
+                  }),
+            ],
           ),
         ),
       );
